e5e54e4c54f67fda1d1f2c6e4dff9b2a2334cd75
baeky <ksc2601@gmail.com>
2023-01-03 20:13:13 +0900

    chore(chart): ifconfig-alias.sh -> ifconfig-alias-up.sh

--- chart/scripts/ifconfig-alias-down.sh
+++ chart/scripts/ifconfig-alias-down.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 NWK_INF=$(ip route get 1.1.1.1 | awk 'NR==1 { print $5 }')
-IP=10.0.0.1
+IP=$($(cat chart-config.yaml | yq '.env.LB_IP_ADDRESS_POOL' | sed "s/\/*//g))
 
 sudo ifconfig $NWK_INF -alias $IP
--- chart/scripts/ifconfig-alias-up.sh
+++ chart/scripts/ifconfig-alias-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NWK_INF=$(ip route get 1.1.1.1 | awk 'NR==1 { print $5 }')
-IP=10.0.0.1/32
+IP=$(cat chart-config.yaml | yq '.env.LB_IP_ADDRESS_POOL')
 
 # ---------------------------------------------------------------------------- #
 #                    내 로컬머신에 고정된 내부 네트워크 고정IP를 할당한다.                  #
