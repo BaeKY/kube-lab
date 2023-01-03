ea4437ea96d69b98cc4de89eae29530a7095f2f1
baeky <ksc2601@gmail.com>
2023-01-03 20:17:41 +0900

    chore(chart): add-custom-dns-resolver.sh

---
+++ chart/scripts/add-custom-dns-resolver.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 원하는 도메인 입력
+# *.local은 기본적으로 사용중이다(IANA). 나는 localhost 로 결정
+DOMAIN=$(cat chart-config.yaml | yq '.env.CUSTOM_DOMAIN')
+
+# 10.0.0.1에 DNS서버를 구축해둔 상태에서
+NS_IP=$(cat chart-config.yaml | yq '.env.NS_IP')
+
+RESOLVER_NAME=$DOMAIN
+RESOLVER_CONTENT="# bobs-lab/kube-ops에 의해 작성되었습니다.
+domain $DOMAIN
+search $DOMAIN
+nameserver $NS_IP
+"
+
+sudo mkdir -p /private/etc/resolver
+echo "$RESOLVER_CONTENT" | sudo tee -a $RESOLVER_NAME &>/dev/null
+
+# https://vninja.net/2020/02/06/macos-custom-dns-resolvers/
+# 위 링크처럼 DNS Refresh
+sudo killall -HUP mDNSResponder
+
+# 등록 확인
+scutil --dns | grep "domain   : $DOMAIN" -A 5 -B 1 | awk '{$1=$1;print}'
